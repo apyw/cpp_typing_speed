cb9d07b92f10cc9894ed697087a07be9d4ddada2
apyw <angiew.py@gmail.com>
2020-08-24 22:08:55 +0800

    Update results screen
    Modified method of calcuating results

--- test.h
+++ test.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <fstream>
 #include <vector>
+#include <string>
 
 class typing_test
 {
@@ -13,26 +14,26 @@ public:
     void results(int, int, int);
 };
 
-
-// results screen 
-void typing_test::results(int cpm, int wpm, int wrong){
+// results screen
+void typing_test::results(int cpm, int wpm, int wrong)
+{
     std::cout << "----------\n"
-		 << "Results\n"
-		 << "----------\n"
-		 << "Characters typed: " << cpm << "\n"
-         << "Words typed: " << wpm <<"\n"
-		 << "Incorrect words: " << wrong << "\n"
-	     << "Press enter to return to the start.\n";
-	std::cin.ignore();
+              << "Results\n"
+              << "----------\n"
+              << "Correct characters typed: " << cpm << "\n"
+              << "Correct words typed: " << wpm << "\n"
+              << "Incorrect words: " << wrong << "\n"
+              << "Press enter to return to the start.\n";
+    std::cin.ignore();
 }
 
-
-void typing_test::start(){
+void typing_test::start()
+{
     int char_typed = 0;
     int words_typed = 0;
     int wrong_words = 0;
     std::string word, answer, display;
-    std::ifstream dict ("words.txt");
+    std::ifstream dict("words.txt");
     std::vector<std::string> words;
     std::chrono::steady_clock::time_point tEnd = std::chrono::steady_clock::now() + std::chrono::seconds(60);
 
@@ -40,27 +41,34 @@ void typing_test::start(){
     std::cin.ignore();
 
     // generates random words
-    if (dict.is_open()){
-        while(getline(dict, word) && (std::chrono::steady_clock::now() <= tEnd)){
+    if (dict.is_open())
+    {
+        while (getline(dict, word) && (std::chrono::steady_clock::now() <= tEnd))
+        {
             words.push_back(word);
             srand(time(NULL));
             display = words[rand() % words.size()];
             std::cout << display << std::endl;
             std::cin >> answer;
-            char_typed = char_typed + (sizeof(word));
-            words_typed = words_typed + 1;
-            
-            // alerts user when incorrect word is typed
-            if(answer != display){
-                wrong_words = wrong_words + 1;
-				std::cout << "\aWrong\n" << std::endl;
-				std::cout << display << std::endl;				
-				std::cin  >> answer;	
-			}	
+
+            if (answer == display)
+            {
+                char_typed += (word.length());
+                std::cout << char_typed;
+                ++words_typed;
+            }
+            else
+            {
+                // alerts user when incorrect word is typed
+                ++wrong_words;
+                std::cout << "\aWrong" << std::endl;
+            }
         }
         dict.close();
         std::cout << "Time is up. These are your results.\n";
         std::cin.ignore();
-		results(char_typed, words_typed, wrong_words);
+        results(char_typed, words_typed, wrong_words);
     }
+
+    return;
 }
